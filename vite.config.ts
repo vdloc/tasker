70c11ab3d83d727d31db5034bff8a68c9c1cfb0c
vdloc <uvcudlco@gmail.com>
2023-05-19 10:52:23 +0700

    build: update configurations for library building

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,23 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'MyLib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
   },
 });
