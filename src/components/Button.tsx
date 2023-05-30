0c4b78247bc1ee0899585f5af5ba24d64779e501
vdloc <uvcudlco@gmail.com>
2023-05-30 10:31:55 +0700

    feat(button): update button component with tailwind variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,58 @@
-type ButtonProps = {
-  label: string
-}
+import { tv } from 'tailwind-variants';
+import type { VariantProps } from 'tailwind-variants';
 
-export default function Button({ label }: ButtonProps) {
-  return <button
-    type='button'
-    className='relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-  >
-    {label}
-  </button>
+const buttonStyleProps = tv({
+  base: 'relative inline-flex items-center border border-transparent text-sm text-white shadow-sm font-medium focus:outline-none',
+  variants: {
+    color: {
+      indigo: 'focus:ring-indigo-500 bg-indigo-600 hover:bg-indigo-700',
+      pink: 'focus:ring-pink-500 bg-pink-600 hover:bg-pink-700',
+    },
+    size: {
+      small: 'px-2 py-1',
+      large: 'px-4 py-2',
+    },
+    rounded: {
+      true: 'rounded-md',
+    },
+    hasRing: {
+      true: 'focus:ring-2 focus:ring-offset-2',
+    },
+  },
+  defaultVariants: {
+    color: 'indigo',
+  },
+});
+
+type OtherButtonProps = {
+  label: string;
+};
+
+type ButtonProps = JSX.IntrinsicElements['button'] &
+  VariantProps<typeof buttonStyleProps> &
+  OtherButtonProps;
+
+export default function Button({
+  label,
+  color,
+  size,
+  rounded,
+  hasRing,
+  className,
+  ...otherProps
+}: ButtonProps) {
+  return (
+    <button
+      type='button'
+      className={`${buttonStyleProps({
+        color,
+        size,
+        rounded,
+        hasRing,
+      })} ${className}`}
+      {...otherProps}
+    >
+      {label}
+    </button>
+  );
 }
