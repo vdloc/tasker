c538e8996851735a98f2bec3ab6d0aaf01c68049
vdloc <uvcudlco@gmail.com>
2023-05-30 15:52:05 +0700

    feat(slidepopup): add slide popup component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import TodoList from './components/TodoList';
+import SlidePopUp from './components/SlidePopup';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,10 +10,11 @@ function App() {
   return (
     <>
       <div className='container mx-auto bg-white'>
-        <div className="flex justify-center items-center min-h-screen">
+        <div className='flex justify-center items-center min-h-screen'>
           <Card title='Todos Maker' description='Improvement at your own way.'>
             <TodoList />
           </Card>
+          <SlidePopUp />
         </div>
       </div>
     </>
