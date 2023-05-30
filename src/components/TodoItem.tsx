634c65c667be753962ec5a86fe0517fbfd632e5f
vdloc <uvcudlco@gmail.com>
2023-05-30 10:33:02 +0700

    feat(todoitem,todolist): replace flex with grid for layout, add edit button for todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,17 @@
+import Button from './Button';
+
 type TodoItem = {
+  id: string | number;
   title: string;
   description: string;
   status: boolean;
   tags?: string[];
-  createTime?: Date;
-  dueTime?: Date;
-  finishTime?: Date;
+  createTime?: string;
+  dueTime?: string;
+  finishTime?: string;
 };
 export default function TodoItem({
+  id,
   title,
   description,
   status,
@@ -16,5 +20,28 @@ export default function TodoItem({
   dueTime,
   finishTime,
 }: TodoItem) {
-  return;
+  return (
+    <div className='relative grid grid-cols-12 items-center py-4'>
+      <div className='col-span-8'>
+        <label
+          htmlFor={`todo-${id}`}
+          className='select-none font-medium text-md text-gray-700 cursor-pointer'
+        >
+          {title}
+        </label>
+        <p className='text-xs text-black mt-1'>{description}</p>
+      </div>
+      <div className='col-span-2 text-right pr-3'>
+        <input
+          id={`todo-${id}`}
+          name={`todo-${id}`}
+          type='checkbox'
+          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 cursor-pointer'
+        />
+      </div>
+      <div className='col-span-2 text-right'>
+        <Button label='Edit' size='small' color='pink' rounded={true} />
+      </div>
+    </div>
+  );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,33 +1,17 @@
 import todos from '../data/todos.json';
+import TodoItem from './TodoItem';
 
 export default function TodoList() {
   return (
     <fieldset>
-      <legend className='font-medium text-gray-900 flex justify-between w-full'>
-        <span className='text-lg'>Tasks</span>
-        <span className='text-md'>Done?</span>
+      <legend className='font-medium text-gray-900 grid grid-cols-12 justify-center w-full'>
+        <span className='text-lg col-span-8'>Tasks</span>
+        <span className='text-md col-span-2 text-right'>Status</span>
+        <span className='text-md col-span-2 text-right'>Action</span>
       </legend>
       <div className='mt-4 divide-y divide-gray-200 border-t border-b border-gray-200 text-left'>
-        {todos.map((todo, todoIndex) => (
-          <div key={todoIndex} className='relative flex items-center justify-between py-4'>
-            <div className='min-w-0 max-w-xs'>
-              <label
-                htmlFor={`todo-${todo.id}`}
-                className='select-none font-medium text-md text-gray-700 cursor-pointer'
-              >
-                {todo.title}
-              </label>
-              <p className='text-xs text-black mt-1'>{todo.description}</p>
-            </div>
-            <div className='ml-3 flex h-5 items-center'>
-              <input
-                id={`todo-${todo.id}`}
-                name={`todo-${todo.id}`}
-                type='checkbox'
-                className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 cursor-pointer'
-              />
-            </div>
-          </div>
+        {todos.map((todo) => (
+          <TodoItem {...todo} key={todo.id} />
         ))}
       </div>
     </fieldset>
