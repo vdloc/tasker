d18bee8257ce175053744d17e416d0c02b2e147e
vdloc <uvcudlco@gmail.com>
2023-05-30 23:36:56 +0700

    feat(types): add types file

---
+++ src/types/index.tsx
@@ -0,0 +1,20 @@
+export type TodoItem = {
+  id: string | number;
+  title: string;
+  description: string;
+  status: boolean;
+  tags?: string[];
+  createTime?: string;
+  dueTime?: string;
+  finishTime?: string;
+};
+
+export interface StoreState {
+  isTaskUpdatePopupOpen: boolean,
+  isTaskCreatePopupOpen: boolean,
+  selectingTask: TodoItem | null,
+  tasks: TodoItem[] | [],
+  setSelectingTask: (task: TodoItem) => void,
+  toggleTaskUpdatePopup: (isOpen: boolean) => void,
+  toggleTaskCreatePopup: (isOpen: boolean) => void
+}
