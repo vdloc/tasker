7b423d4be4bd45ba83d2cbf9ca5bac8125f00b5d
vdloc <uvcudlco@gmail.com>
2023-05-31 16:05:02 +0700

    feat(store): add immer middleware to store creation

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,32 @@
-import { create } from "zustand";
-import type { StoreState, TodoItem } from "./types";
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import type { StoreState, TodoItem } from './types';
 
-export const useStore = create<StoreState>((set) => ({
-  tasks: [],
-  selectingTask: null,
-  isTaskUpdatePopupOpen: false,
-  isTaskCreatePopupOpen: false,
-  toggleTaskUpdatePopup: (isOpen = false) =>
-    set(() => ({
-      isTaskUpdatePopupOpen: isOpen,
-    })),
-  toggleTaskCreatePopup: (isOpen = false) =>
-    set(() => ({
-      isTaskCreatePopupOpen: isOpen,
-    })),
-  setSelectingTask: (task: TodoItem) => set(() => ({ selectingTask: task })),
-}));
+export const useStore = create<StoreState, [['zustand/immer', never]]>(
+  immer((set) => ({
+    tasks: [],
+    selectingTask: null,
+    isTaskUpdatePopupOpen: false,
+    isTaskCreatePopupOpen: false,
+    toggleTaskUpdatePopup: (isOpen = false) =>
+      set(() => ({
+        isTaskUpdatePopupOpen: isOpen,
+      })),
+    toggleTaskCreatePopup: (isOpen = false) =>
+      set(() => ({
+        isTaskCreatePopupOpen: isOpen,
+      })),
+    setSelectingTask: (task: TodoItem) => set(() => ({ selectingTask: task })),
+    updateTask: (updatedTask: TodoItem) =>
+      set((state: StoreState) => {
+        const updatingTaskIndex = state.tasks.findIndex(
+          (task) => task.id === updatedTask.id
+        );
+        state.tasks = [
+          ...state.tasks.slice(0, updatingTaskIndex),
+          updatedTask,
+          ...state.tasks.slice(updatingTaskIndex + 1),
+        ];
+      }),
+  }))
+);
