02f479e89aef188e78d803cd6ee6088098b71fe7
vdloc <uvcudlco@gmail.com>
2023-05-31 16:07:48 +0700

    feat(dialogpopup): refactor dialog popup components
    Clean up struture, move header into separated file

---
+++ src/components/DialogPopup/Header.tsx
@@ -0,0 +1,37 @@
+import { Dialog } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
+type DialogPopupHeaderProps = {
+  onClose: (isOpen: boolean) => void;
+  title: string;
+  description: string;
+};
+
+export default function DialogPopUpHeader({
+  onClose,
+  title,
+  description,
+}: DialogPopupHeaderProps) {
+  return (
+    <div className='bg-indigo-700 py-6 px-4 sm:px-6'>
+      <div className='flex items-center justify-between'>
+        <Dialog.Title className='text-lg font-medium text-white'>
+          {title}
+        </Dialog.Title>
+        <div className='ml-3 flex h-7 items-center'>
+          <button
+            type='button'
+            className='rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white'
+            onClick={() => onClose(false)}
+          >
+            <span className='sr-only'>Close panel</span>
+            <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+          </button>
+        </div>
+      </div>
+      <div className='mt-1'>
+        <p className='text-sm text-indigo-300'>{description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/DialogPopup/index.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment, JSX } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { useStore } from '@/store';
+import TaskEditForm from '../TaskEditForm';
+
+type DialogPopUpProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: JSX.Element;
+};
+
+export default function DialogPopup({
+  isOpen,
+  onClose,
+  children,
+}: DialogPopUpProps) {
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog
+        as='div'
+        className='fixed w-screen h-screen top-0 left-0 z-20'
+        onClose={onClose}
+      >
+        <div className='bg-indigo-400 opacity-40 absolute w-full h-full'></div>
+        <div className='pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[25rem] h-[700px] rounded-lg z-30 shadow-xl'>
+          <Transition.Child
+            as={Fragment}
+            enter='transform transition ease-in-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-1'
+            leave='transform transition ease-in-out duration-300'
+            leaveFrom='opacity-1'
+            leaveTo='opacity-0'
+          >
+            <Dialog.Panel className='pointer-events-auto h-full'>
+              {children}
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
