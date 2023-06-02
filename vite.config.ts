9b682fb99a108ecd639a52a84c6a653605ff9389
vdloc <uvcudlco@gmail.com>
2023-06-02 15:08:00 +0700

    build(vite.config): remove build config

--- vite.config.ts
+++ vite.config.ts
@@ -11,24 +11,6 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
-      name: 'MyLib',
-      formats: ['es', 'umd'],
-      fileName: (format) => `my-lib.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'styled-components': 'styled',
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
