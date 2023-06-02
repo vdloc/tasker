f8f239d5f3c484ec52667eadcdeef5a9c63c7c0c
vdloc <uvcudlco@gmail.com>
2023-06-02 15:17:33 +0700

    feat(app): change app name and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
-import TodoList from './components/TodoList';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className='container mx-auto bg-white'>
         <div className='flex justify-center items-center min-h-screen'>
-          <Card
-            title='Todos Maker'
-            description='Improvement at your own way.'
-          />
+          <Card title='The TooDoo' description='Achieve your goals' />
         </div>
       </div>
     </>
