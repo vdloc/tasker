93970396d3aaedada6e093e02397fc00393b1f9a
vdloc <uvcudlco@gmail.com>
2023-06-02 15:17:08 +0700

    style(dialogpopup): reformat

--- src/components/DialogPopup/index.tsx
+++ src/components/DialogPopup/index.tsx
@@ -1,7 +1,5 @@
-import React, { Fragment, JSX } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { useStore } from "@/store";
-import TaskEditForm from "../TaskEditForm";
+import { Fragment, JSX } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 type DialogPopUpProps = {
   isOpen: boolean;
@@ -17,22 +15,22 @@ export default function DialogPopup({
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
-        as="div"
-        className="fixed w-screen h-screen top-0 left-0 z-20"
+        as='div'
+        className='fixed w-screen h-screen top-0 left-0 z-20'
         onClose={onClose}
       >
-        <div className="bg-indigo-400 opacity-40 absolute w-full h-full"></div>
-        <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[25rem] rounded-lg z-30 shadow-xl">
+        <div className='bg-indigo-400 opacity-40 absolute w-full h-full'></div>
+        <div className='pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[25rem] rounded-lg z-30 shadow-xl'>
           <Transition.Child
             as={Fragment}
-            enter="transform transition ease-in-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-1"
-            leave="transform transition ease-in-out duration-300"
-            leaveFrom="opacity-1"
-            leaveTo="opacity-0"
+            enter='transform transition ease-in-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-1'
+            leave='transform transition ease-in-out duration-300'
+            leaveFrom='opacity-1'
+            leaveTo='opacity-0'
           >
-            <Dialog.Panel className="pointer-events-auto h-full">
+            <Dialog.Panel className='pointer-events-auto h-full'>
               {children}
             </Dialog.Panel>
           </Transition.Child>
