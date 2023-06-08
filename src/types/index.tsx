2d76eef01a16de197e948b99f515263617c62533
vdloc <uvcudlco@gmail.com>
2023-06-08 22:25:19 +0700

    feat(type): revert control type to any

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -64,7 +64,7 @@ export type TagsListEditFormValues = {
 };
 
 export type FormInputProps = {
-  control: Control<TaskEditFormValues | TagsListEditFormValues>;
+  control: Control<any>;
   name: any;
   label: string;
   id?: string;
