f49e7d3dc65f796165dbba56847a5a1a8e4f37b9
vdloc <uvcudlco@gmail.com>
2023-08-06 14:03:03 +0700

    style(main): add global toast classes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import './index.css';
-import router from './routes/router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 
+import './index.css';
+import router from './routes/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <Toaster />
-  </React.StrictMode>
+    <Toaster
+      toastOptions={{
+        className: 'text-sm font-medium',
+      }}
+    />
+  </React.StrictMode>,
 );
